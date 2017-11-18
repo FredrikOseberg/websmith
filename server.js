7389e5bbd84534db823acf73f730ef11c2c78a9e
FredrikOseberg <fredrik.no@gmail.com>
2017-11-18 11:19:03 +1000

    Added mailchimp api

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
+const addUserToEmailList = require('./api/mailchimp');
 
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/home.html');
+});
+
+app.post('/addtoemail', (req, res) => {
+	// const email = res.query.email;
+	addUserToEmailList('fredrik.ose@gmail.com');
 });
 
 app.listen(3001, () => {
