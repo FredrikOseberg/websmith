f7badb5e3d50e7729805c56ecbcbdb4cd37ea779
FredrikOseberg <fredrik.no@gmail.com>
2017-11-18 12:13:38 +1000

    Set mailchimp api as config var and remove gitignore

---
+++ api/mailchimp.js
@@ -0,0 +1,23 @@
+var request = require('request');
+
+function callback(error, response, body) {
+	if (!error && response.statusCode == 200) {
+		console.log('Success');
+	}
+}
+
+module.exports = email => {
+	var options = {
+		url: 'https://us9.api.mailchimp.com/3.0/lists/11d80d6cd8/members/',
+		method: 'POST',
+		auth: {
+			user: 'anystring',
+			pass: process.env.mailchimpApi
+		},
+		body: JSON.stringify({
+			email_address: email,
+			status: 'subscribed'
+		})
+	};
+	request(options, callback);
+};
