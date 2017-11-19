9ecd4ed35299e91e57417f1931e7088b4d64786b
FredrikOseberg <fredrik.no@gmail.com>
2017-11-19 18:41:15 +1000

    Remove IP filter

--- index.js
+++ index.js
@@ -3,15 +3,10 @@ const app = express();
 const port = process.env.PORT || 8080;
 const cors = require('cors');
 const addUserToEmailList = require('./api/mailchimp');
-const ipFilter = require('express-ipfilter').IpFilter;
-
-const ips = ['151.101.65.195', '151.101.1.195'];
 
 const corsOptions = {
 	origin: 'https://cryptodasher.com'
 };
-
-app.use('/addtoemail', ipFilter(ips, { mode: 'allow' }));
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
