f63323230b2a97ac76045d4c4b94f5bbfa4bc917
FredrikOseberg <fredrik.no@gmail.com>
2017-11-19 17:02:42 +1000

    Change cors settings on endpoint

--- index.js
+++ index.js
@@ -2,18 +2,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 const addUserToEmailList = require('./api/mailchimp');
-const cors = require('cors');
-
-const corsOptions = {
-	origin: 'https://cryptodasher.com/',
-	optionsSuccessStatus: 200
-};
-
-app.use(function(req, res, next) {
-	res.header('Access-Control-Allow-Origin', 'https://cryptodasher.com');
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-	next();
-});
 
 app.use(express.static(__dirname));
 
@@ -21,7 +9,13 @@ app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/home.html');
 });
 
-app.post('/addtoemail', cors(corsOptions), (req, res) => {
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', 'https://cryptodasher.com');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
+});
+
+app.post('/addtoemail', (req, res) => {
 	const email = req.query.email;
 	if (email) {
 		addUserToEmailList(email);
