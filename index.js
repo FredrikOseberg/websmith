ace1c745ecdd8542b4c0a0dbfb355e09ab97ffc6
FredrikOseberg <fredrik.no@gmail.com>
2017-11-19 17:37:51 +1000

    Remove cors options

--- index.js
+++ index.js
@@ -6,23 +6,11 @@ const addUserToEmailList = require('./api/mailchimp');
 
 app.use(express.static(__dirname));
 
-var corsOptions = {
-	origin: 'https://cryptodasher.com',
-	optionsSuccessStatus: 200
-};
-
-app.use(function(req, res, next) {
-	res.header('Access-Control-Allow-Origin', 'https://cryptodasher.com');
-	res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-	next();
-});
-
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/home.html');
 });
 
-app.post('/addtoemail', cors(corsOptions), (req, res, next) => {
+app.post('/addtoemail', cors(), (req, res, next) => {
 	const email = req.query.email;
 	if (email) {
 		addUserToEmailList(email);
