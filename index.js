cc88db37382203edca3bf38061f9171f1bf501ee
FredrikOseberg <fredrik.no@gmail.com>
2017-11-19 18:47:29 +1000

    Try new request headers

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
-const cors = require('cors');
 const addUserToEmailList = require('./api/mailchimp');
 
-const corsOptions = {
-	origin: 'https://cryptodasher.com'
-};
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', 'https://cryptodasher.com');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
+});
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/home.html');
 });
 
-app.post('/addtoemail', cors(corsOptions), (req, res, next) => {
+app.post('/addtoemail', (req, res, next) => {
 	const email = req.query.email;
 	if (email) {
 		addUserToEmailList(email);
