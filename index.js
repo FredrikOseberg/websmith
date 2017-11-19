eaa3cf9631095b7610e3cee181e7c24372034bbd
FredrikOseberg <fredrik.no@gmail.com>
2017-11-19 16:59:04 +1000

    Add cors header middleware

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 const addUserToEmailList = require('./api/mailchimp');
+const cors = require('cors');
+
+const corsOptions = {
+	origin: 'https://cryptodasher.com/',
+	optionsSuccessStatus: 200
+};
+
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', 'https://cryptodasher.com');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
+});
 
 app.use(express.static(__dirname));
 
@@ -9,15 +21,10 @@ app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/home.html');
 });
 
-app.post('/addtoemail', (req, res) => {
-	const origin = req.get('origin');
-	if (origin === 'https://cryptodasher.com') {
-		const email = req.query.email;
-		if (email) {
-			addUserToEmailList(email);
-		}
-	} else {
-		res.send('You are not authorized to perform this action');
+app.post('/addtoemail', cors(corsOptions), (req, res) => {
+	const email = req.query.email;
+	if (email) {
+		addUserToEmailList(email);
 	}
 });
 
