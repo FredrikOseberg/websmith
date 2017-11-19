09ab59efeeb9564d5feaba126b914e54046de174
FredrikOseberg <fredrik.no@gmail.com>
2017-11-19 17:09:07 +1000

    Attempt with cors middleware

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 const addUserToEmailList = require('./api/mailchimp');
+const cors = require('cors');
 
 app.use(express.static(__dirname));
 
+app.use(
+	cors({
+		origin: 'https://cryptodasher.com'
+	})
+);
+
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/home.html');
-});
-
-app.use(function(req, res, next) {
-	res.header('Access-Control-Allow-Origin', 'https://cryptodasher.com');
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-	next();
 });
 
 app.post('/addtoemail', (req, res) => {
