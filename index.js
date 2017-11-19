498d124c55b0ad0211b114655594868e7ae6b951
FredrikOseberg <fredrik.no@gmail.com>
2017-11-19 19:10:56 +1000

    Added origin to be cryptodasher

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const addUserToEmailList = require('./api/mailchimp');
 const cors = require('cors');
 
 const corsOptions = {
-	origin: false,
+	origin: 'https://cryptodasher.com',
 	optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
