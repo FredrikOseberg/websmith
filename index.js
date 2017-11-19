7369bb8b96f0de8c2578c8096ecf5f0bec1a8023
FredrikOseberg <fredrik.no@gmail.com>
2017-11-19 18:26:49 +1000

    Enable cors middleware

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
+const cors = require('cors');
 const addUserToEmailList = require('./api/mailchimp');
 const ipFilter = require('express-ipfilter').IpFilter;
 
 const ips = ['151.101.65.195', '151.101.1.195'];
+
+const corsOptions = {
+	origin: 'https://cryptodasher.com'
+};
 
 app.use('/addtoemail', ipFilter(ips, { mode: 'allow' }));
 app.use(express.static(__dirname));
@@ -13,7 +18,7 @@ app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/home.html');
 });
 
-app.post('/addtoemail', (req, res, next) => {
+app.post('/addtoemail', cors(corsOptions), (req, res, next) => {
 	const email = req.query.email;
 	if (email) {
 		addUserToEmailList(email);
